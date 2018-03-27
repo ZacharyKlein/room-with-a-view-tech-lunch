cfca2985db20a383984241d60bf2799761b29a55
Zachary Klein <zak@silver-chalice.com>
2018-03-27 00:25:42 -0500

    Cleanup

--- server/grails-app/init/demo/BootStrap.groovy
+++ server/grails-app/init/demo/BootStrap.groovy
@@ -15,11 +15,14 @@ class BootStrap {
 
     def init = { servletContext ->
         log.info "Populating database"
-        Author ken = authorService.save("Ken Kousen")
+        Author ken = authorService.save "Ken Kousen"
+        Author eric = authorService.save "Eric Helgeson"
+        Author cris = authorService.save "Cristian Olaru"
 
         bookService.save "Making Java Groovy", 800, ken
         bookService.save "Gradle Recipes", 500, ken
-        bookService.save "Android Recipies", 600, ken
+        bookService.save "Practical Grails 3", 600, eric
+        bookService.save "Grails 3 - Step by Step", 600, cris
 
 
     }
